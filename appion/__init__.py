e882546d54888d7de07b042f54b6fc6fadd3c366
Jorge Jiménez de la Morena <54907551+JorMaister@users.noreply.github.com>
2026-01-27 16:13:38 +0100

    Bump version from 3.0.3 to 3.0.4

--- appion/__init__.py
+++ appion/__init__.py
@@ -33,7 +33,7 @@ import pwem
 from pyworkflow.utils import Environ
 from .constants import DOGPICKER_HOME, V0_2_1_1
 
-__version__ = '3.0.3'
+__version__ = '3.0.4'
 _references = ['Voss2009']
 _logo = 'appion_logo.png'
 
